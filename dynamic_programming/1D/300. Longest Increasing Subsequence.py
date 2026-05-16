f77d9a40c54657ee56c56eb80a2ebb4822cf21c2
yardicous <tayyuda.work@gmail.com>
2026-05-16 16:47:21 +0800

    new

---
+++ dynamic_programming/1D/300. Longest Increasing Subsequence.py
@@ -0,0 +1,42 @@
+# Longest Increasing Subsequence
+# Medium Topics Company Tags
+# Hints
+#
+# Given an integer array nums, return the length of the longest strictly increasing subsequence.
+#
+# A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements without changing the relative order of the remaining characters.
+#
+#     For example, "cat" is a subsequence of "crabt".
+
+# Solution
+# Nested loops and dp search, tried to do it with pruning but didnt work out
+# Also through this problem learn to set dp values based on the question requirement at first put as float but had to put as 1 as all positions is a subarray of 1 initially
+
+# TC: O(N ^ 2)
+# SC: O(N)
+
+from typing import List
+class BottomUp:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        dp = [1] * N
+        dp[-1] = 1
+        for i in range(N - 1,-1,-1):
+            for j in range(i + 1,N):    
+                if nums[j] > nums[i]:
+                    dp[i] = max(dp[i],dp[j] + 1)
+        return max(dp)
+
+
+from functools import lru_cache
+class TopDown:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        @lru_cache(None)
+        def dfs(i):
+            res = 1
+            for j in range(i + 1,N):
+                if nums[j] > nums[i]:
+                    res = max(res,dfs(j) + 1)
+            return res
+        return max(dfs(i) for i in range(N))
---
+++ dynamic_programming/1D/3186. Maximum Total Damage With Spell Casting.py
@@ -0,0 +1,59 @@
+# A magician has various spells.
+#
+# You are given an array power, where each element represents the damage of a spell. Multiple spells can have the same damage value.
+#
+# It is a known fact that if a magician decides to cast a spell with a damage of power[i], they cannot cast any spell with a damage of power[i] - 2, power[i] - 1, power[i] + 1, or power[i] + 2.
+#
+# Each spell can be cast only once.
+#
+# Return the maximum possible total damage that a magician can cast.
+
+# Solution, at first I thought I can solve it greedily but turns out it was dynamic programming
+
+# TC: O(N)
+# SC: O(N)
+
+
+from collections import defaultdict
+from functools import lru_cache
+from typing import List
+class TopDown:
+    def maximumTotalDamage(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        for n in nums:
+            count[n] += n
+        power = sorted(count.keys())
+        N = len(power)
+        @lru_cache(None)
+        def dfs(i):
+            if i == N:
+                return 0
+            res = dfs(i + 1)
+            take = count[power[i]]
+            j = i + 1
+            while j < N and power[j] - power[i] <= 2:
+                j += 1
+            take += dfs(j)
+            res = max(res,take)
+            return res
+        r = dfs(0)
+        dfs.cache_clear()
+        return r
+class BottomUp:
+    def maximumTotalDamage(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        for n in nums:
+            count[n] += n
+        power = sorted(count.keys())
+        N = len(power)
+        dp = [10 ** 20] * (N + 1)
+        dp[-1] = 0
+        for i in range(N - 1,-1,-1):
+            take = count[power[i]]
+            j = i + 1
+            while j < N and power[j] - power[i] <= 2:
+                j += 1
+            take += dp[j]
+            dp[i] = max(take,dp[i + 1])
+        return dp[0]
+
