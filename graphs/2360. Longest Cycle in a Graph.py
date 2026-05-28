9e6b68abedb3b71a81581189c6250763a9b49ed0
yardicous <tayyuda.work@gmail.com>
2026-05-28 19:01:24 +0800

    new

---
+++ graphs/2360. Longest Cycle in a Graph.py
@@ -0,0 +1,65 @@
+# You are given a directed graph of n nodes numbered from 0 to n - 1, where each node has at most one outgoing edge.
+#
+# The graph is represented with a given 0-indexed array edges of size n, indicating that there is a directed edge from node i to node edges[i]. If there is no outgoing edge from node i, then edges[i] == -1.
+#
+# Return the length of the longest cycle in the graph. If no cycle exists, return -1.
+#
+# A cycle is a path that starts and ends at the same node.
+
+# Solution
+# I have to keep track of local map, previously I did not do that and TLE because of that
+
+# TC: O(N)
+# SC: O(N)
+
+from typing import List
+from collections import deque
+class BFS:
+    def longestCycle(self, edges: List[int]) -> int:
+        N = len(edges)
+        res = -1
+        visit = [False] * N
+        
+        def bfs(node):
+            q = deque()
+            q.append((0,node))
+            local_visit = {}
+            while q:
+                depth,index = q.popleft()
+                if index in local_visit != -1:
+                    nonlocal res
+                    res = max(res,depth - local_visit[index])
+                if visit[index]:
+                    continue
+                visit[index] = True
+                local_visit[index] = depth
+                if edges[index] != -1:
+                    q.append((depth + 1,edges[index]))
+
+        for i in range(N):
+            if not visit[i] and edges[i] != -1:
+                bfs(i)
+        return res
+class DFS:
+    def longestCycle(self, edges: List[int]) -> int:
+        N = len(edges)
+        res = -1
+        visit = [False] * N
+        
+        def dfs(node,m,depth):
+            if node == -1:
+                return
+            if node in m:
+                nonlocal res
+                res = max(res,depth - m[node])
+                return
+            if visit[node]:
+                return
+            visit[node] = True
+            m[node] = depth
+            dfs(edges[node],m,depth + 1)
+
+        for i in range(N):
+            if not visit[i]:
+                dfs(i,{},0)
+        return res
---
+++ greedy/3026. Maximum Good Subarray Sum.py
@@ -0,0 +1,32 @@
+# You are given an array nums of length n and a positive integer k.
+#
+# A subarray  of nums is called good if the absolute difference between its first and last element is exactly k, in other words, the subarray nums[i..j] is good if |nums[i] - nums[j]| == k.
+#
+# Return the maximum sum of a good subarray of nums. If there are no good subarrays, return 0.
+
+# Solution
+# The idea for prefix sum is basically finding if n + k and n - k exists in our prefix if it is, take the running total and subtract away the ones we do not want
+# the catch is when duplicates, we want to find the minimum suffix sum of the value since the question wants the maximum total subarray
+
+# TC: O(N)
+# SC: O(N)
+
+from typing import List
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        INF = 10 ** 20
+        res = -INF
+        total = 0
+        prefix = {}
+        for n in nums:
+            if n in prefix:
+                prefix[n] = min(prefix[n],total)
+            else:
+                prefix[n] = total
+            total += n
+            for y in [n + k,n - k]:
+                if y in prefix:
+                    res = max(res,total - prefix[y])
+        return res if res != -INF else 0
+
+        
