85b0202fd24fdb04144ee6a94ec4d615cf74f38a
yardicous <tayyuda.work@gmail.com>
2026-06-01 17:01:28 +0800

    new

---
+++ dynamic_programming/1D/2054. Two Best Non-Overlapping Events.py
@@ -0,0 +1,63 @@
+# You are given a 0-indexed 2D integer array of events where events[i] = [startTimei, endTimei, valuei]. The ith event starts at startTimei and ends at endTimei, and if you attend this event, you will receive a value of valuei. You can choose at most two non-overlapping events to attend such that the sum of their values is maximized.
+#
+# Return this maximum sum.
+#
+# Note that the start time and end time is inclusive: that is, you cannot attend two events where one of them starts and the other ends at the same time. More specifically, if you attend an event with end time t, the next event must start at or after t + 1.
+
+# Solution
+# DP + Binary search, another way to do is sweep line with a suffix, basically when entering a new segment check previous segments max value
+
+# TC: O(N log N)
+# SC: O(N)
+
+from typing import List
+from functools import lru_cache
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        N = len(events)
+        events.sort(key=lambda x:(x[0],x[1]))
+        # print(events)
+        def good(idx,target):
+            left,right = idx + 1,N - 1
+            res = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if events[mid][0] > target:
+                    right = mid - 1
+                    res = mid
+                else:
+                    left = mid + 1
+            return res
+        @lru_cache(100005)
+        def dfs(i,count):
+            if i == N or count == 0:
+                return 0
+            # skip
+            res = dfs(i + 1,count)
+            end = events[i][1]
+            idx = good(i,end)
+            # print(idx)
+            take = events[i][2]
+            if idx != -1:
+                take += dfs(idx,count - 1)
+            res = max(res,take)
+            return res
+        r = dfs(0,2)
+        dfs.cache_clear()
+        return r
+class SweepLine:
+    def maxTwoEvents(self, e: List[List[int]]) -> int:
+        events = []
+        for start,end,value in e:
+            events.append((start,value,1))
+            events.append((end,value,-1))
+        events.sort(key=lambda x:(x[0],-x[2]))
+
+        res = 0
+        mx_left = 0
+        for time,val,choice in events:
+            if choice == 1:
+                res = max(res,val + mx_left)
+            else:
+                mx_left = max(mx_left,val)
+        return res
---
+++ graphs/2039. The Time When the Network Becomes Idle.py
@@ -0,0 +1,50 @@
+# There is a network of n servers, labeled from 0 to n - 1. You are given a 2D integer array edges, where edges[i] = [ui, vi] indicates there is a message channel between servers ui and vi, and they can pass any number of messages to each other directly in one second. You are also given a 0-indexed integer array patience of length n.
+#
+# All servers are connected, i.e., a message can be passed from one server to any other server(s) directly or indirectly through the message channels.
+#
+# The server labeled 0 is the master server. The rest are data servers. Each data server needs to send its message to the master server for processing and wait for a reply. Messages move between servers optimally, so every message takes the least amount of time to arrive at the master server. The master server will process all newly arrived messages instantly and send a reply to the originating server via the reversed path the message had gone through.
+#
+# At the beginning of second 0, each data server sends its message to be processed. Starting from second 1, at the beginning of every second, each data server will check if it has received a reply to the message it sent (including any newly arrived replies) from the master server:
+#
+#     If it has not, it will resend the message periodically. The data server i will resend the message every patience[i] second(s), i.e., the data server i will resend the message if patience[i] second(s) have elapsed since the last time the message was sent from this server.
+#     Otherwise, no more resending will occur from this server.
+#
+# The network becomes idle when there are no messages passing between servers or arriving at servers.
+#
+# Return the earliest second starting from which the network becomes idle.
+
+# Solution
+# Just a bfs but I need to remember the formula which is time - 1 // p * p the reason we -1 is because we want the last possible time when an email has been sent and since it comes
+# in intervals of p -1 gets the last time when it send out
+# TC: O(N)
+# SC: O(N)
+
+from collections import defaultdict,deque
+from typing import List
+class Solution:
+    def networkBecomesIdle(self, edges: List[List[int]], patience: List[int]) -> int:
+        adj_list = defaultdict(list)
+        for v,u in edges:
+            adj_list[v].append(u)
+            adj_list[u].append(v)
+        q = deque()
+        q.append((0,0))
+        visit = set()
+        res = 0
+        visit.add(0)
+        while q:
+            node,dist = q.popleft()
+            if node != 0:
+                p = patience[node]
+                time = dist * 2
+                if time > p:
+                    amount = (time - 1) // p * p
+                    res = max(res,amount + time + 1)
+                else:
+                    res = max(res,time + 1)
+            for neiNode in adj_list[node]:
+                if neiNode not in visit:
+                    q.append((neiNode,dist + 1))
+                    visit.add(neiNode)
+        return res
+                    
