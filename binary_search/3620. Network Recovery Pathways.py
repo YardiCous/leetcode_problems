3e9b4604fc1a0babde382bc1a4c075af3e38c176
yardicous <tayyuda.work@gmail.com>
2026-06-18 23:01:31 +0800

    new

---
+++ binary_search/3620. Network Recovery Pathways.py
@@ -0,0 +1,58 @@
+# You are given a directed acyclic graph of n nodes numbered from 0 to n − 1. This is represented by a 2D array edges of length m, where edges[i] = [ui, vi, costi] indicates a one‑way communication from node ui to node vi with a recovery cost of costi.
+#
+# Some nodes may be offline. You are given a boolean array online where online[i] = true means node i is online. Nodes 0 and n − 1 are always online.
+#
+# A path from 0 to n − 1 is valid if:
+#
+#     All intermediate nodes on the path are online.
+#     The total recovery cost of all edges on the path does not exceed k.
+#
+# For each valid path, define its score as the minimum edge‑cost along that path.
+#
+# Return the maximum path score (i.e., the largest minimum-edge cost) among all valid paths. If no valid path exists, return -1.
+
+
+# Solution
+# Binary search on answer + Dijsktra, I misread the question and I did not think of states, definitely need to come back on this
+
+# TC: O(N log N)
+# SC: O(N)
+
+class Solution:
+    def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
+        N = len(online)
+        adj_list = defaultdict(list)
+        mx = 0
+        for v,u,cost in edges:
+            adj_list[v].append((u,cost))
+            mx = max(cost,mx)
+        
+        INF = 10 ** 20
+        def good(target):
+            dist = [INF] * N
+            dist[0] = 0
+            minHeap = [(0,0)]
+            while minHeap:
+                # print(minHeap)
+                total,node = heapq.heappop(minHeap)
+                if node == N - 1:
+                    return total <= k
+                if total > dist[node]:
+                    continue
+                for neiNode,neiCost in adj_list[node]:
+                    if online[neiNode] and neiCost >= target:    
+                        if dist[neiNode] > neiCost + total:
+                            dist[neiNode] = neiCost + total
+                            heapq.heappush(minHeap,(neiCost + total,neiNode))
+            return False
+    
+        left,right = 0,mx + 1
+        res = -1
+        while left < right:
+            mid = (left + right) // 2
+            if good(mid):
+                left = mid + 1
+                res = mid
+            else:
+                right = mid
+        return res
---
+++ graphs/3669. Balanced K-Factor Decomposition.py
@@ -0,0 +1,42 @@
+# Given two integers n and k, split the number n into exactly k positive integers such that the product of these integers is equal to n.
+#
+# Return any one split in which the maximum difference between any two numbers is minimized. You may return the result in any order.
+
+# Solution
+# Kind of cheated in the sense I saw k is <= 5 and I knew I can do brute force 
+
+# TC: O(N * Sqrt(N))
+class Solution:
+    def minDifference(self, n: int, k: int) -> List[int]:
+        q = deque()
+        q.append([n])
+        visit = set()
+        s = [n]
+        visit.add(tuple(s))
+        def factors(x):
+            res = set()
+            for i in range(1,int(x ** 0.5) + 1):
+                if x % i == 0:
+                    res.add(i)
+            return res
+        res = 10 ** 20
+        r = []
+        while q:
+            # print(q)
+            nums = q.popleft()
+            for i,num in enumerate(nums):
+                f = factors(num)
+                for j in f:
+                    new = []
+                    new.append(j)
+                    new.append(num // j)
+                    new = nums[:i] + new + nums[i + 1:]
+                    if len(new) <= k and tuple(new) not in visit:
+                        if len(new) == k:
+                            mx,mn = max(new),min(new)
+                            if mx - mn < res:
+                                res = mx - mn
+                                r = new[:]
+                        q.append(new[:])
+                        visit.add(tuple(new))
+        return r
