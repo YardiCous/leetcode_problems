9a2f6a2c261b1e08ae41daff07029db1a8330933
yardicous <tayyuda.work@gmail.com>
2026-06-21 17:04:16 +0800

    new

---
+++ graphs/3965. Finish Time of Tasks I.py
@@ -0,0 +1,69 @@
+# You are given an integer n representing the number of tasks in a project, numbered from 0 to n - 1. These tasks are connected as a tree rooted at task 0. This is represented by a 2D integer array edges of length n - 1, where edges[i] = [ui, vi] indicates that task ui is the parent of task vi.
+#
+# You are also given an array baseTime of length n, where baseTime[i] represents the time to complete task i.
+#
+# The finish time of each task is calculated as follows:
+#
+#     Leaf task: The finish time is baseTime[i].
+#     Non-leaf task:
+#         Let earliest be the minimum finish time among its children, and latest be the maximum finish time among its children.
+#         Let ownDuration be (latest - earliest) + baseTime[i].
+#         The finish time of task i is latest + ownDuration.
+#
+# Return the finish time of the root task 0.
+
+# Solution
+# Just DFS or Topo Sort
+
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def finishTime(self, N: int, edges: List[List[int]], baseTime: List[int]) -> int:
+        INF = 10 ** 20
+        adj_list = defaultdict(list)
+        for v,u in edges:
+            adj_list[v].append(u)
+            adj_list[u].append(v)
+        def dfs(node,par):
+            mx,mn = -INF,INF
+            for neiNode in adj_list[node]:
+                if neiNode != par:
+                    t = dfs(neiNode,node)
+                    mx = max(mx,t)
+                    mn = min(mn,t)
+            if mx == -INF and mn == INF:
+                return baseTime[node]
+            own_duration = mx - mn + baseTime[node]
+            return own_duration + mx
+        return dfs(0,-1)
+
+class Solution:
+    def finishTime(self, N: int, edges: List[List[int]], baseTime: List[int]) -> int:
+        adj_list = defaultdict(list)
+        degree = [0] * N
+        for v,u in edges:
+            adj_list[u].append(v)
+            degree[v] += 1
+        q = deque()
+        dist = [0] * N
+        for i in range(N):
+            if degree[i] == 0:
+                q.append(i)
+                dist[i] = baseTime[i]
+        visit = defaultdict(list)
+        mx = [-inf] * N
+        mn = [inf] * N
+        while q:
+            node = q.popleft()
+            for neiNode in adj_list[node]:
+                
+                mx[neiNode] = max(mx[neiNode],dist[node])
+                mn[neiNode] = min(mn[neiNode],dist[node])
+                l,e = mx[neiNode],mn[neiNode]
+                dist[neiNode] = l - e + baseTime[neiNode] + l
+                degree[neiNode] -= 1
+                if degree[neiNode] == 0:
+                    q.append(neiNode)
+        return dist[0]
+            
---
+++ graphs/3970. Shortest Path With At Most K Consecutive Identical Characters.py
@@ -0,0 +1,38 @@
+# You are given an integer n representing the number of nodes in a directed weighted graph, numbered from 0 to n - 1. This is represented by a 2D integer array edges, where edges[i] = [ui, vi, wi] represents a directed edge from node ui to node vi with weight wi.
+#
+# You are also given a string labels of length n, where labels[i] is the character assigned to node i, and an integer k.
+#
+# Return the minimum total edge weight of a path from node 0 to node n - 1 such that the concatenation of the labels of the nodes along the path contains at most k consecutive identical characters. If no valid path exists, return -1.
+
+# Solution
+# Dijkstra
+
+# TC: O(N log N * k)
+# SC: O(N * k)
+class Solution:
+    def shortestPath(self, N: int, edges: List[List[int]], labels: str, k: int) -> int:
+        INF = 10 ** 20
+        adj_list = defaultdict(list)
+        mx = 0
+        for v,u,w in edges:
+            adj_list[v].append((u,w))
+            mx = max(mx,w)  
+        minHeap = [(0,0,0)]
+        visit = set()
+        while minHeap:
+            cost,node,consec = heapq.heappop(minHeap)
+            if node in visit:
+                continue
+            if node == N - 1:
+                return cost
+            visit.add(node)
+            for neiNode,neiCost in adj_list[node]:
+                if neiNode not in visit:
+                    if labels[neiNode] != labels[node]:
+                        heapq.heappush(minHeap,(cost + neiCost,neiNode,0))
+                    elif labels[neiNode] == labels[node]:
+                        if consec + 1 < k:
+                            heapq.heappush(minHeap,(cost + neiCost,neiNode,consec + 1))
+            
+
+        return -1
