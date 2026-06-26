bf4c6a34f490cb5e5ac1da9d9c17dab91890a9b0
yardicous <tayyuda.work@gmail.com>
2026-06-26 18:31:51 +0800

    new

---
+++ maths/3002. Maximum Size of a Set After Removals.py
@@ -0,0 +1,48 @@
+# You are given two 0-indexed integer arrays nums1 and nums2 of even length n.
+#
+# You must remove n / 2 elements from nums1 and n / 2 elements from nums2. After the removals, you insert the remaining elements of nums1 and nums2 into a set s.
+#
+# Return the maximum possible size of the set s.
+
+# Solution
+# Just Math 
+
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def maximumSetSize(self, nums1: List[int], nums2: List[int]) -> int:
+        N = len(nums1)
+        common = 0
+        freq = defaultdict(int)
+        freq1 = defaultdict(int)
+        freq2 = defaultdict(int)
+        def helper(f,limit):
+            nonlocal common
+            res = 0
+            for x in f:
+                if limit <= 0:
+                    break
+                limit -= 1
+                res += 1
+            while limit > 0 and common > 0:
+                limit -= 1
+                common -= 1
+                res += 1
+            return res
+        for x in set(nums1):
+            freq[x] += 1
+            freq1[x] += 1
+        for x in set(nums2):
+            freq[x] += 1
+            freq2[x] += 1
+        
+        for x in freq:
+            if freq[x] == 2:
+                common += 1
+                freq1[x] -= 1
+                del freq1[x]
+                freq2[x] -= 1 
+                del freq2[x]
+        limit = N // 2
+        return helper(freq1,limit) + helper(freq2,limit)
---
+++ sliding_window/2831. Find the Longest Equal Subarray.py
@@ -0,0 +1,54 @@
+# You are given a 0-indexed integer array nums and an integer k.
+#
+# A subarray is called equal if all of its elements are equal. Note that the empty subarray is an equal subarray.
+#
+# Return the length of the longest possible equal subarray after deleting at most k elements from nums.
+#
+# A subarray is a contiguous, possibly empty sequence of elements within an array.
+
+# Solution
+# Sliding window, but I did not think of O(N) solution, seems like i overcomplicated this solution, used SortedList and added a log factor
+
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def longestEqualSubarray(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        freq = defaultdict(int)
+        left = 0
+        res = 0
+        for right in range(N):
+            x = nums[right]
+            freq[x] += 1
+            res = max(res,freq[x])
+            while (right - left + 1) - res > k:
+                freq[nums[left]] -= 1
+                if freq[nums[left]] == 0:
+                    del freq[nums[left]]
+                left += 1
+        return res
+class Solution:
+    def longestEqualSubarray(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        freq = defaultdict(int)
+        left = 0
+        res = 0
+        sl = SortedList()
+        for right in range(N):
+            x = nums[right]
+            if (freq[x],x) in sl:
+                sl.remove((freq[x],x))
+            freq[x] += 1
+            sl.add((freq[x],x))
+            while (right - left + 1) - sl[-1][0] > k:
+                l = nums[left]
+                sl.remove((freq[l],l))
+                freq[l] -= 1
+                if freq[l] > 0:
+                    sl.add((freq[l],l))
+                elif freq[l] == 0:
+                    del freq[l]
+                left += 1
+            res = max(res,sl[-1][0])
+        return res
