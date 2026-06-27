e5dc3b0705bfdc6d4c8302fe33ff7ee1eb856189
yardicous <tayyuda.work@gmail.com>
2026-06-27 10:36:40 +0800

    new

---
+++ greedy/3020. Find the Maximum Number of Elements in Subset.py
@@ -0,0 +1,42 @@
+You are given an array of positive integers nums.
+
+You need to select a of nums which satisfies the following condition:
+
+    You can place the selected elements in a 0-indexed array such that it follows the pattern: [x, x2, x4, ..., xk/2, xk, xk/2, ..., x4, x2, x] (Note that k can be be any non-negative power of 2). For example, [2, 4, 16, 4, 2] and [3, 9, 3] follow the pattern while [2, 4, 8, 4, 2] does not.
+
+Return the maximum number of elements in a subset that satisfies these conditions.
+
+# Solution
+# Needed help
+
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        N = len(nums)
+        res = 1
+        freq = defaultdict(int)
+        for x in nums:
+            freq[x] += 1
+        if 1 in freq:
+            if freq[1] % 2 == 1:
+                res = max(res,freq[1])
+            else:
+                res = max(res,freq[1] - 1)
+        for x in freq:
+            if x == 1:
+                continue
+            curr = x
+            length = 0
+            while curr in freq and freq[curr] >= 2:
+                length += 2
+                curr *= curr
+            if curr in freq and freq[curr] == 1:
+                length += 1
+            else:
+                length -= 1
+            res = max(res,length)
+        return res
+
+            
---
+++ string/3081. Replace Question Marks in String to Minimize Its Value.py
@@ -0,0 +1,58 @@
+You are given a string s. s[i] is either a lowercase English letter or '?'.
+
+For a string t having length m containing only lowercase English letters, we define the function cost(i) for an index i as the number of characters equal to t[i] that appeared before it, i.e. in the range [0, i - 1].
+
+The value of t is the sum of cost(i) for all indices i.
+
+For example, for the string t = "aab":
+
+    cost(0) = 0
+    cost(1) = 1
+    cost(2) = 0
+    Hence, the value of "aab" is 0 + 1 + 0 = 1.
+
+Your task is to replace all occurrences of '?' in s with any lowercase English letter so that the value of s is minimized.
+
+Return a string denoting the modified string with replaced occurrences of '?'. If there are multiple strings resulting in the minimum value, return the one.
+
+
+# Solution
+# Just ad hoc but i couldn't come up with the lexicographically smallest string 
+
+# TC:O(N)
+# SC:O(N)
+
+class Solution:
+    def minimizeStringValue(self, s: str) -> str:
+        N = len(s)
+        freq = [0] * 26
+        chosen = [0] * 26
+        count = 0
+        res = list(s)
+        for char in s:
+            if char == "?":
+                count += 1
+                continue
+            val = ord(char) - ord("a")
+            freq[val] += 1
+        if count == 0:
+            return "".join(res)
+        INF = 10 ** 20
+        while count:
+            mn = INF
+            index = -1
+            for j in range(26):
+                if freq[j] < mn:
+                    mn = freq[j]
+                    index = j
+            freq[index] += 1
+            count -= 1
+            chosen[index] += 1
+        j = 0
+        for i in range(N):
+            if res[i] == "?":
+                while chosen[j] == 0:
+                    j += 1
+                res[i] = chr(j + ord("a"))
+                chosen[j] -= 1
+        return "".join(res)
