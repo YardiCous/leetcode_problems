f35ec0eb085434f62bb829a2a920eb94d004f4db
yardicous <tayyuda.work@gmail.com>
2026-06-30 19:31:51 +0800

    new

---
+++ binary_search/2861. Maximum Number of Alloys.py
@@ -0,0 +1,38 @@
+You are the owner of a company that creates alloys using various types of metals. There are n different types of metals available, and you have access to k machines that can be used to create alloys. Each machine requires a specific amount of each metal type to create an alloy.
+
+For the ith machine to create an alloy, it needs composition[i][j] units of metal of type j. Initially, you have stock[i] units of metal type i, and purchasing one unit of metal type i costs cost[i] coins.
+
+Given integers n, k, budget, a 1-indexed 2D array composition, and 1-indexed arrays stock and cost, your goal is to maximize the number of alloys the company can create while staying within the budget of budget coins.
+
+All alloys must be created with the same machine.
+
+Return the maximum number of alloys that the company can create.
+
+# Solution
+# Binary search on answer treating the answer as the highest number we can make
+
+# TC: O(N * M Log K)
+# SC: O(1)
+
+class Solution:
+    def maxNumberOfAlloys(self, n: int, k: int, budget: int, composition: List[List[int]], stock: List[int], cost: List[int]) -> int:
+        def good(target):
+            for i in range(k):
+                curr = 0
+                for j in range(n):
+                    required = target * composition[i][j]
+                    if required > stock[j]:
+                        curr += (required - stock[j]) * cost[j]
+                if curr <= budget:
+                    return True
+            return False
+        left,right = 0,10 ** 9 + 1
+        res = 0
+        while left < right:
+            mid = (left + right) // 2
+            if good(mid):
+                res = mid
+                left = mid + 1
+            else:
+                right = mid
+        return res
---
+++ graphs/2872. Maximum Number of K-Divisible Components.py
@@ -0,0 +1,35 @@
+There is an undirected tree with n nodes labeled from 0 to n - 1. You are given the integer n and a 2D integer array edges of length n - 1, where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the tree.
+
+You are also given a 0-indexed integer array values of length n, where values[i] is the value associated with the ith node, and an integer k.
+
+A valid split of the tree is obtained by removing any set of edges, possibly empty, from the tree such that the resulting components all have values that are divisible by k, where the value of a connected component is the sum of the values of its nodes.
+
+Return the maximum number of components in any valid split.
+
+# Solution
+# DFS 
+
+# TC : O(N)
+# SC: O(N)
+
+class Solution:
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        adj_list = defaultdict(list)
+        for v,u in edges:
+            adj_list[v].append(u)
+            adj_list[u].append(v)
+        res = 0
+        def dfs(node,par):
+            s = values[node]
+            for neiNode in adj_list[node]:
+                if neiNode == par:
+                    continue
+                s += dfs(neiNode,node)
+            if s % k == 0:
+                nonlocal res
+                res += 1
+                return 0
+            return s
+        dfs(0,-1)
+        return res
+        
