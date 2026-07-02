78cb05ce22e9b9331809b96337902444d2ec0aac
yardicous <tayyuda.work@gmail.com>
2026-07-02 20:14:35 +0800

    new

---
+++ design/2296. Design a Text Editor.py
@@ -0,0 +1,81 @@
+Design a text editor with a cursor that can do the following:
+
+    Add text to where the cursor is.
+    Delete text from where the cursor is (simulating the backspace key).
+    Move the cursor either left or right.
+
+When deleting text, only characters to the left of the cursor will be deleted. The cursor will also remain within the actual text and cannot be moved beyond it. More formally, we have that 0 <= cursor.position <= currentText.length always holds.
+
+Implement the TextEditor class:
+
+    TextEditor() Initializes the object with empty text.
+    void addText(string text) Appends text to where the cursor is. The cursor ends to the right of text.
+    int deleteText(int k) Deletes k characters to the left of the cursor. Returns the number of characters actually deleted.
+    string cursorLeft(int k) Moves the cursor to the left k times. Returns the last min(10, len) characters to the left of the cursor, where len is the number of characters to the left of the cursor.
+    string cursorRight(int k) Moves the cursor to the right k times. Returns the last min(10, len) characters to the left of the cursor, where len is the number of characters to the left of the cursor.
+
+# Solution
+# Doubly Linked List, not that good at it
+
+# TC: O(K) for all
+# SC: O(N) where N is the maximum number of text
+
+
+class Node:
+    def __init__(self,char = None):
+        self.char = char
+        self.prev = self.next = None
+class TextEditor:
+    def __init__(self):
+        self.head = Node()
+        self.current = self.head
+    def addText(self, text: str) -> None:
+        old_next = self.current.next
+        for s in text:
+            self.current.next = Node(s)
+            self.current.next.prev = self.current
+            self.current = self.current.next
+        if old_next is not None:
+            self.current.next = old_next
+            old_next.prev = self.current
+    def deleteText(self, k: int) -> int:
+        count = 0
+        for _ in range(k):
+            if self.current.char is None:
+                break
+            prev = self.current.prev
+            nxt = self.current.next
+            prev.next = nxt
+            if nxt is not None:
+                nxt.prev = prev
+            self.current = prev
+            count += 1
+        return count
+
+    def cursorLeft(self, k: int) -> str:
+        for _ in range(k):
+            if self.current.prev is not None:
+                self.current = self.current.prev
+        return self.helper()
+
+    def cursorRight(self, k: int) -> str:
+        for _ in range(k):
+            if self.current.next is not None:
+                self.current = self.current.next
+        return self.helper()
+    def helper(self) -> str:
+        res = []
+        curr = self.current
+        for _ in range(10):
+            if curr and curr.char is not None:
+                res.append(curr.char)
+                curr = curr.prev
+        return "".join(reversed(res))
+
+
+# Your TextEditor object will be instantiated and called as such:
+# obj = TextEditor()
+# obj.addText(text)
+# param_2 = obj.deleteText(k)
+# param_3 = obj.cursorLeft(k)
+# param_4 = obj.cursorRight(k)
---
+++ graphs/3286. Find a Safe Walk Through a Grid.py
@@ -0,0 +1,66 @@
+You are given an m x n binary matrix grid and an integer health.
+
+You start on the upper-left corner (0, 0) and would like to get to the lower-right corner (m - 1, n - 1).
+
+You can move up, down, left, or right from one cell to another adjacent cell as long as your health remains positive.
+
+Cells (i, j) with grid[i][j] = 1 are considered unsafe and reduce your health by 1.
+
+Return true if you can reach the final cell with a health value of 1 or more, and false otherwise.
+
+# Solution
+# 0-1 BFS or Djikstra
+
+# TC: O(N) for BFS O(N Log N) for djikstra
+# SC: O(N)
+
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        rows,cols = len(grid),len(grid[0])
+        directions = [
+            (1,0),(-1,0),(0,1),(0,-1)
+        ]
+        INF = 10 ** 20
+        q = deque()
+        q.append((0,0))
+        dist = [[INF] * cols for _ in range(rows)]
+        dist[0][0] = grid[0][0]
+        while q:
+            r,c = q.popleft()
+            for dx,dy in directions:
+                nx,ny = dx + r,dy + c
+                if 0 <= nx < rows and 0 <= ny < cols and dist[nx][ny] == INF:
+                    delta = grid[nx][ny]
+                    if delta + dist[r][c] < health:
+                        if delta:
+                            q.append((nx,ny))
+                        else:
+                            q.appendleft((nx,ny))
+                        dist[nx][ny] = delta + dist[r][c]
+        return dist[-1][-1] < health
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        rows,cols = len(grid),len(grid[0])
+        directions = [
+            (1,0),(-1,0),(0,1),(0,-1)
+        ]
+        if grid[0][0] == 1:
+            health -= 1
+        
+        pq = [(-health,0,0)]
+        visit = set()
+        while pq:
+            curr,r,c = heapq.heappop(pq)
+            curr *= -1
+            if r == rows - 1 and c == cols - 1:
+                return True
+            if (r,c) in visit:
+                continue
+            visit.add((r,c))
+            for dx,dy in directions:
+                nx,ny = dx + r, dy + c
+                if 0 <= nx < rows and 0 <= ny < cols and (nx,ny) not in visit:
+                    new_curr = curr - grid[nx][ny]
+                    if new_curr > 0:
+                        heapq.heappush(pq,(-new_curr,nx,ny))
+        return False
