21205ae93eda70c832fe32d9fbf7dd3e561988fc
yardicous <tayyuda.work@gmail.com>
2026-08-11 22:57:31 +0800

    new

---
+++ binary_search/4016. Maximum Area of Two Non-Overlapping Square Submatrices.py
@@ -0,0 +1,66 @@
+You are given a 2D integer matrix mat of size m × n, where:
+
+    mat[r][c] == 1 means the cell at row r and column c is usable.
+    mat[r][c] == 0 means it is not usable.
+
+Your task is to find two that satisfy the following conditions:
+
+    Both submatrices must be squares of the same side length k.
+    The two submatrices must not share any cell.
+    Each submatrix can only cover cells where mat[r][c] == 1.
+
+Return the maximum possible area of each of the two squares. If it is not possible to choose two such squares, return 0.
+
+# Solution
+# Binary search on answer + DP 
+
+# TC: O(N * Log(N))
+# SC: O(N)
+# N = 500 * 500
+class Solution:
+    def maxArea(self, mat: List[List[int]]) -> int:
+        rows,cols = len(mat),len(mat[0])
+        dp = [[0] * cols for _ in range(rows)]
+        for c in range(cols):
+            dp[-1][c] = mat[-1][c]
+        for r in range(rows):
+            dp[r][-1] = mat[r][-1]
+        for x in range(rows - 2,-1,-1):
+            for y in range(cols - 2,-1,-1):
+                if mat[x][y] == 1:
+                    dp[x][y] = min(dp[x + 1][y],dp[x][y + 1],dp[x + 1][y + 1]) + 1
+        # print(dp)
+        def good(k):
+            r = SortedList()
+            c = SortedList()
+            for x in range(rows):
+                for y in range(cols):
+                    if dp[x][y] == k:
+                        r.add(x)
+                        c.add(y)
+            for index,x in enumerate(r):
+                if r[0] <= x - k or r[-1] >= x + k:
+                    if index != 0 and r[0] <= x - k:
+                        return True
+                    if index != len(r) - 1 and r[-1] >= x + k:
+                        return True
+            for index,y in enumerate(c):
+                if c[0] <= x - k or c[-1] >= y + k:
+                    if index != 0 and c[0] <= y - k:
+                        return True
+                    if index != len(c) - 1 and c[-1] >= y + k:
+                        return True
+            return False
+                
+        left = 0
+        right = (rows * cols) // 2
+        res = 0
+        while left <= right:
+            mid = (left + right + 1) // 2
+            if good(mid):
+                res = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return res ** 2
+
---
+++ graphs/886. Possible Bipartition.py
@@ -0,0 +1,35 @@
+We want to split a group of n people (labeled from 1 to n) into two groups of any size. Each person may dislike some other people, and they should not go into the same group.
+
+Given the integer n and the array dislikes where dislikes[i] = [ai, bi] indicates that the person labeled ai does not like the person labeled bi, return true if it is possible to split everyone into two groups in this way.
+
+
+# Solution
+# 2 Coloring Greedy BFS/DFS problem, if a problem requires spliting into 2 groups, use this algorithm
+
+# TC: O(V + E)
+# SC: O(V + E)
+
+class Solution:
+    def possibleBipartition(self, N: int, dislikes: List[List[int]]) -> bool:
+        adj_list = defaultdict(list)
+        for a,b in dislikes:
+            a -= 1
+            b -= 1
+            adj_list[a].append(b)
+            adj_list[b].append(a)
+        color = [-1] * N
+        for i in range(N):
+            if color[i] != -1:
+                continue
+            color[i] = 0
+            q = deque()
+            q.append(i)
+            while q:
+                node = q.popleft()
+                for neiNode in adj_list[node]:
+                    if color[neiNode] == -1:
+                        q.append(neiNode)
+                        color[neiNode] = not color[node]
+                    elif color[neiNode] == color[node]:
+                        return False
+        return True
