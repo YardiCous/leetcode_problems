6f5c2bb06a8e396400c0ef37346c057cf20f1dba
yardicous <tayyuda.work@gmail.com>
2026-08-20 17:41:30 +0800

    new

---
+++ dynamic_programming/palindrome/3504. Longest Palindrome After Substring Concatenation II.py
@@ -0,0 +1,70 @@
+You are given two strings, s and t.
+
+You can create a new string by selecting a from s (possibly empty) and a substring from t (possibly empty), then concatenating them in order.
+
+Return the length of the longest that can be formed this way.
+
+# Solution
+# Larry helped with it
+
+class Solution:
+    def longestPalindrome(self, s: str, t: str) -> int:
+        N = len(s)
+        M = len(t)
+        t = t[::-1]
+        INF = 10 ** 20
+        @cache
+        def longest_s2(left,right):
+            if left > right:
+                return 0
+            if left == right:
+                return 1
+            if s[left] == s[right]:
+                return 2 + longest_s2(left + 1,right - 1)
+            return -INF
+        @cache
+        def longest_s(left,right):
+            if left > right:
+                return 0
+            return max(longest_s(left,right - 1),longest_s2(left,right))
+        @cache
+        def longest_t2(left,right):
+            if left > right:
+                return 0
+            if left == right:
+                return 1
+            if t[left] == t[right]:
+                return 2 + longest_t2(left + 1,right - 1)
+            return -INF
+        @cache
+        def longest_t(left,right):
+            if left > right:
+                return 0
+            return max(longest_t(left,right - 1),longest_t2(left,right))
+        @cache 
+        def f(i,j):
+            if i == N:
+                return longest_t(j,M - 1)
+            if j == M:
+                return longest_s(i,N - 1)
+            res = 0
+            if s[i] == t[j]:
+                res = max(res,2 + f(i + 1,j + 1))
+                res = max(res,2 + longest_t(j + 1,M - 1))
+                res = max(res,2 + longest_s(i + 1,N - 1))
+            return res
+        res = 0
+        for i in range(N):
+            res = max(res,longest_s(i,N - 1))
+
+        for i in range(M):
+            res = max(res,longest_t(i,M - 1))
+        for i in range(N):
+            for j in range(M):
+                res = max(res,f(i,j))
+        f.cache_clear()
+        longest_t.cache_clear()
+        longest_s.cache_clear()
+        longest_t2.cache_clear()
+        longest_s2.cache_clear()
+        return res
---
+++ segments/3072. Distribute Elements Into Two Arrays II.py
@@ -0,0 +1,51 @@
+You are given a 1-indexed array of integers nums of length n.
+
+We define a function greaterCount such that greaterCount(arr, val) returns the number of elements in arr that are strictly greater than val.
+
+You need to distribute all the elements of nums between two arrays arr1 and arr2 using n operations. In the first operation, append nums[1] to arr1. In the second operation, append nums[2] to arr2. Afterwards, in the ith operation:
+
+    If greaterCount(arr1, nums[i]) > greaterCount(arr2, nums[i]), append nums[i] to arr1.
+    If greaterCount(arr1, nums[i]) < greaterCount(arr2, nums[i]), append nums[i] to arr2.
+    If greaterCount(arr1, nums[i]) == greaterCount(arr2, nums[i]), append nums[i] to the array with a lesser number of elements.
+    If there is still a tie, append nums[i] to arr1.
+
+The array result is formed by concatenating the arrays arr1 and arr2. For example, if arr1 == [1,2,3] and arr2 == [4,5,6], then result = [1,2,3,4,5,6].
+
+Return the integer array result.
+
+
+# Solution
+# Pretty straight forward
+
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        N = len(nums)
+        sl = SortedList()
+        sl2 = SortedList()
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
+
+        sl.add(nums[0])
+        sl2.add(nums[1])
+        def greaterCount(s,val):
+            idx = s.bisect_right(val)
+            return len(s) - idx
+        for i in range(2,N):
+            count1 = greaterCount(sl,nums[i])
+            count2 = greaterCount(sl2,nums[i])
+            # print(count1,count2)
+            if count1 > count2:
+                arr1.append(nums[i])
+                sl.add(nums[i])
+            elif count1 < count2:
+                arr2.append(nums[i])
+                sl2.add(nums[i])
+            elif count1 == count2:
+                if len(arr1) <= len(arr2):
+                    arr1.append(nums[i])
+                    sl.add(nums[i])
+                else:
+                    arr2.append(nums[i])
+                    sl2.add(nums[i])
+            # print(arr1,arr2)
+        return arr1 + arr2
